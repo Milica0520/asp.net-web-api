9530902cbf03f502d4b10591e1f8a519a1aded6f
Milica <dujakovic.milica3@gmail.com>
2024-09-16 13:01:12 +0200

    homework - try out PostMan and Swagger

--- BookLibraryApi/BookLibraryApi/obj/Release/net8.0/BookLibraryApi.AssemblyInfo.cs
+++ BookLibraryApi/BookLibraryApi/obj/Release/net8.0/BookLibraryApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLibraryApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3894b069cd27f007eae4974d2b8736662700cae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLibraryApi")]
